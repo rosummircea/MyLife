8ba3343a7bc35053bdd09689ff82740a762e510e
rosummircea <rosu.m.mircea@gmail.com>
2026-09-17 10:38:52 +0300

    Add Salt Edge fail callback route

---
+++ app/api/open-banking/saltedge/fail/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest } from "next/server";
+import { handleSaltEdgeCallback } from "@/lib/open-banking/saltedge/callback";
+
+export async function POST(request: NextRequest) {
+  return handleSaltEdgeCallback(request, "fail");
+}
