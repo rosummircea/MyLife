11a66d0fe680661eea1397cb798d0b857cc31b0b
rosummircea <rosu.m.mircea@gmail.com>
2026-09-17 06:15:14 +0300

    Show real React preview when Supabase deployment config is missing

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import DemoApp from '@/components/DemoApp'
 import MyLifeApp from '@/components/MyLifeApp'
 
 export default function Page() {
-  return <MyLifeApp />
+  const hasSupabaseConfig = Boolean(
+    process.env.NEXT_PUBLIC_SUPABASE_URL &&
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+  )
+
+  return hasSupabaseConfig ? <MyLifeApp /> : <DemoApp />
 }
